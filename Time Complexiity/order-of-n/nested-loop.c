45d33160713572a25e3bf87130530eb0d4e9c61b
Salman Farshe <salman.zihad@involve.ai>
2024-02-11 08:47:47 +0600

    Time Complexity

---
+++ Time Complexiity/order-of-n/nested-loop.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int n = 10;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+       for (int j = 0; j < n; j++){
+            count++;
+        }
+    }
+    printf("n = 2 then count is 4\n");
+    printf("n = 6 then count is 36\n");
+    printf("n = 10 then count is 100\n");
+    printf("n = n then count is n*n\n");
+    printf("Complexity for result = %d is O(n^2)", count);
+        return 0;
+}
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SalmanFarshe/Algorithms.git
+git push -u origin main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
